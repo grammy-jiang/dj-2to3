260f6320ec45dab70b67689e5dff2b186b14ea6d
Grammy Jiang <grammy.jiang@gmail.com>
2024-11-17 00:15:40 +1100

    Add an empty project for dj-2to3

---
+++ config/settings/__init__.py
@@ -0,0 +1,7 @@
+"""All settings."""
+
+import django_stubs_ext
+
+from .django import *
+
+django_stubs_ext.monkeypatch()
---
+++ tests/settings.py
@@ -0,0 +1,3 @@
+"""The settings for test only."""
+
+from config.settings import *  # pylint: disable=wildcard-import,unused-wildcard-import
