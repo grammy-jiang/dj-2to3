e359c53b0a55e7cd77072833258e37d670dfe41e
Grammy Jiang <grammy.jiang@gmail.com>
2024-11-17 00:29:14 +1100

    Add an empty application of dj-2to3

---
+++ dj_2to3/apps.py
@@ -0,0 +1,10 @@
+"""The application configuration of this application."""
+
+from django.apps import AppConfig
+
+
+class Dj2To3Config(AppConfig):
+    """The application configuration of this application."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dj_2to3"
