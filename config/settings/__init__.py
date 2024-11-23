c274272e672eab73ca7677708829e1308d6701d7
Grammy Jiang <grammy.jiang@gmail.com>
2024-11-23 22:12:02 +1100

    Add the settings of dj_2to3 to config.settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,6 +2,7 @@
 
 import django_stubs_ext
 
+from .dj_2to3 import *
 from .django import *
 from .third_parties import *
 
---
+++ config/settings/dj_2to3.py
@@ -0,0 +1,5 @@
+"""All settings of dj_2to3."""
+
+from .django import INSTALLED_APPS
+
+INSTALLED_APPS.append("dj_2to3")
