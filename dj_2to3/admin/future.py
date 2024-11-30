63cda36dc32c46dc737ce955d0960a09d91731dc
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-01 01:52:45 +1100

    Add Inline of PythonExecutable to the admin of Future

--- dj_2to3/admin/future.py
+++ dj_2to3/admin/future.py
@@ -1,8 +1,43 @@
 """The admin of the model of Future."""
 
-from django.contrib import admin
+from typing import Optional
 
-from ..models import Future
+from django.contrib import admin
+from django.http import HttpRequest
+
+from ..models import Future, PythonExecutable
+
+
+class PythonExecutableInline(
+    admin.StackedInline[PythonExecutable, Future]
+):  # pylint: disable=unsubscriptable-object
+    """The PythonExecutable inline."""
+
+    model = PythonExecutable
+
+    def has_add_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[PythonExecutable] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the add permission."""
+        return False
+
+    def has_delete_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[PythonExecutable] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the delete permission."""
+        return False
+
+    def has_change_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[PythonExecutable] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the change permission."""
+        return False
 
 
 @admin.register(Future)
@@ -10,3 +45,7 @@ class FutureAdmin(
     admin.ModelAdmin[Future]
 ):  # pylint: disable=too-few-public-methods,unsubscriptable-object
     """The Future admin."""
+
+    inlines = [
+        PythonExecutableInline,
+    ]
