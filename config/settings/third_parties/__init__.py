8c2e7d56694a4c7bf272215ab9dadb1799c837d4
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-01 00:36:59 +1100

    Add django-versionfield to INSTALLED_APPS

--- config/settings/third_parties/__init__.py
+++ config/settings/third_parties/__init__.py
@@ -3,3 +3,4 @@
 from .browser_reload import *
 from .debug_toolbar import *
 from .extensions import *
+from .versionfield import *
---
+++ config/settings/third_parties/versionfield.py
@@ -0,0 +1,5 @@
+"""The settings of django-versionfield extensions."""
+
+from ..django import INSTALLED_APPS
+
+INSTALLED_APPS.append("versionfield")
