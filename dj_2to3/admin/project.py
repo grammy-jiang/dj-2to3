884271a7a98bf5a356d2634ee736e97c6a96e21c
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-04 10:39:43 +1100

    Update the admin of Project

--- dj_2to3/admin/project.py
+++ dj_2to3/admin/project.py
@@ -1,8 +1,45 @@
 """The admin of the models about Project."""
 
-from django.contrib import admin
+from typing import Optional
 
-from ..models import Project
+from django.contrib import admin
+from django.http import HttpRequest
+
+from ..models import Project, ProjectFix
+
+
+class ProjectFixInline(
+    admin.TabularInline[ProjectFix, Project]
+):  # pylint: disable=unsubscriptable-object
+    """The inline for the project_fix model."""
+
+    fields = ["fix", "diff", "created", "modified"]
+    model = ProjectFix
+    readonly_fields = ["created", "modified"]
+
+    def has_add_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[ProjectFix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the add permission."""
+        return False
+
+    def has_change_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[ProjectFix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the change permission."""
+        return False
+
+    def has_delete_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[ProjectFix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the delete permission."""
+        return False
 
 
 @admin.register(Project)
@@ -12,6 +49,7 @@ class ProjectAdmin(
     """The admin of the models about Project."""
 
     change_form_template = "dj_2to3/change_form_project.html"
+    inlines = (ProjectFixInline,)
     list_display = (
         "path",
         "is_git_repository",
