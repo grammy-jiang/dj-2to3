0ead7ea8d7a62f821f2245c5d6d56846d30908dc
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-04 11:31:14 +1100

    Add a field of syntax_highlight_diff to the admin of ProjectFix

--- dj_2to3/admin/project_fix.py
+++ dj_2to3/admin/project_fix.py
@@ -2,8 +2,13 @@
 
 from typing import Optional
 
+from pygments import highlight
+from pygments.formatters import HtmlFormatter  # pylint: disable=no-name-in-module
+from pygments.lexers import DiffLexer  # pylint: disable=no-name-in-module
+
 from django.contrib import admin
 from django.http import HttpRequest
+from django.utils.html import format_html
 
 from ..models import ProjectFix
 
@@ -14,8 +19,13 @@ class ProjectFixAdmin(
 ):  # pylint: disable=too-few-public-methods,unsubscriptable-object
     """The PythonExecutable admin."""
 
+    fieldsets = (
+        (None, {"fields": ("project", "fix")}),
+        ("Syntax Highlight Diff", {"fields": ("syntax_highlight_diff",)}),
+        ("Time", {"fields": ("created", "modified")}),
+    )
     list_display = ("project", "fix__name", "created", "modified")
-    readonly_fields = ("created", "modified")
+    readonly_fields = ("syntax_highlight_diff", "created", "modified")
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         """Disable the add permission."""
@@ -36,3 +46,13 @@ class ProjectFixAdmin(
     ) -> bool:
         """Disable the delete permission."""
         return False
+
+    @admin.display(description="Diff")
+    def syntax_highlight_diff(self, obj: ProjectFix) -> str | None:
+        """Return the syntax highlight diff."""
+        diff = highlight(
+            obj.diff,
+            DiffLexer(),
+            HtmlFormatter(nobackground=True, noclasses=True),
+        )
+        return format_html(diff)
