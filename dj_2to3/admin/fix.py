646b4a8dfe54109a68632287fbeb3483f9010d1a
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-04 23:08:45 +1100

    Update the admin of Fix

--- dj_2to3/admin/fix.py
+++ dj_2to3/admin/fix.py
@@ -1,6 +1,9 @@
 """The admin of the model of Fix."""
 
+from typing import Optional
+
 from django.contrib import admin
+from django.http import HttpRequest
 
 from ..models import Fix
 
@@ -14,3 +17,27 @@ class FixAdmin(
     list_display = ("name", "category", "created", "modified")
     list_filter = ("category",)
     readonly_fields = ("created", "modified")
+
+    def has_add_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[Fix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the add permission."""
+        return False
+
+    def has_change_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[Fix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the change permission."""
+        return False
+
+    def has_delete_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[Fix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the delete permission."""
+        return False
