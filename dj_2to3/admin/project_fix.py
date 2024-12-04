ab4c78041f4a66e894edf50f8f7460eb47dce7b5
Grammy Jiang <grammy.jiang@gmail.com>
2024-12-04 11:09:28 +1100

    Update the admin of ProjectFix

--- dj_2to3/admin/project_fix.py
+++ dj_2to3/admin/project_fix.py
@@ -1,6 +1,9 @@
 """The admin of the models about Python."""
 
+from typing import Optional
+
 from django.contrib import admin
+from django.http import HttpRequest
 
 from ..models import ProjectFix
 
@@ -10,3 +13,26 @@ class ProjectFixAdmin(
     admin.ModelAdmin[ProjectFix]
 ):  # pylint: disable=too-few-public-methods,unsubscriptable-object
     """The PythonExecutable admin."""
+
+    list_display = ("project", "fix__name", "created", "modified")
+    readonly_fields = ("created", "modified")
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        """Disable the add permission."""
+        return False
+
+    def has_change_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[ProjectFix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the change permission."""
+        return False
+
+    def has_delete_permission(
+        self,
+        request: HttpRequest,
+        obj: Optional[ProjectFix] = None,  # pylint: disable=unused-argument
+    ) -> bool:
+        """Disable the delete permission."""
+        return False
